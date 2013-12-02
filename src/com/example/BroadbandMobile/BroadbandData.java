08acfa6b35c32f634c7bc757d62372a933165043
julius88 <julius.leppala@gmail.com>
2013-12-02 11:26:57 +0200

    jee

--- src/com/example/BroadbandMobile/BroadbandData.java
+++ src/com/example/BroadbandMobile/BroadbandData.java
@@ -3,162 +3,80 @@ package com.example.BroadbandMobile;
 
 import java.util.ArrayList;
 
-/**
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
- * @generated
- */
-
 public class BroadbandData
 {
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
 	private int _xtype;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public int _ytype;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public String _xlabel;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public String _ylabel;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public String _graphtype;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
+    private int _ytype;
+    private String _xlabel;
+    private String _ylabel;
+    private int _graphtype;
 	private ArrayList<BroadbandPoint> _data;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public BroadbandPoint broadbandPoint;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public BroadbandGrapher broadbandGrapher;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 */
+    private BroadbandGrapher broadbandGrapher;
+
+    public final int LINEGRAPH = 1;
+    public final int BARGRAPH = 2;
+
 	public BroadbandData(){
 		super();
 	}
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public void setType(int type) {
-		// TODO : to implement	
+    public int getXtype() {
+        return _xtype;
+    }
+
+    public void setXtype(int _xtype) {
+        this._xtype = _xtype;
+    }
+
+    public int getYtype() {
+        return _ytype;
+    }
+
+    public void setYtype(int _ytype) {
+        this._ytype = _ytype;
+    }
+
+    public String getXlabel() {
+        return _xlabel;
+    }
+
+    public void setXlabel(String _xlabel) {
+        this._xlabel = _xlabel;
+    }
+
+    public String getYlabel() {
+        return _ylabel;
+    }
+
+    public void setYlabel(String _ylabel) {
+        this._ylabel = _ylabel;
+    }
+
+    public int getGraphType() {
+        return _graphtype;
+    }
+
+    public void setGraphType(int _graphtype) {
+        this._graphtype = _graphtype;
+    }
+
+    public void addPoint(BroadbandPoint point) {
+		this._data.add(point);
 	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public int getType() {
-		// TODO : to implement
-		return 0;	
+
+	public void addPoint(double x, double y) {
+		BroadbandPoint point = new BroadbandPoint();
+        point.setX(x);
+        point.setY(y);
+        this._data.add(point);
 	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public void addPoint(BroadbandPoint point) {
-		// TODO : to implement	
-	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public void addPoint(int x, int y) {
-		// TODO : to implement	
-	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
+
 	public int size() {
-		// TODO : to implement
-		return 0;	
+		return this._data.size();
 	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
+
 	public void clear() {
-		// TODO : to implement	
+		this._data.clear();
 	}
-	
 }
 
--- src/com/example/BroadbandMobile/BroadbandPoint.java
+++ src/com/example/BroadbandMobile/BroadbandPoint.java
@@ -1,95 +1,28 @@
 package com.example.BroadbandMobile;
 
-
-/**
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
- * @generated
- */
-
 public class BroadbandPoint
 {
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
 	private double _x;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
 	private double _y;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public BroadbandData broadbandData;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 */
+
 	public BroadbandPoint(){
 		super();
 	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
 	
 	public double getX() {
-		// TODO : to implement
-		return 0.0;	
+		return this._x;
 	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
+
 	public void setX(double x) {
-		// TODO : to implement	
+		this._x = x;
 	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
+
 	public double getY() {
-		// TODO : to implement
-		return 0.0;	
+		return this._y;
 	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
+
 	public void setY(double y) {
-		// TODO : to implement	
+		this._y = y;
 	}
-	
 }
 
